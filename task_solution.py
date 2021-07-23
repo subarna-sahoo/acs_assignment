3abcd8652645549e7bc0bb50c39040edd459336c
Subarna Sahoo <48875593+subarna-sahoo@users.noreply.github.com>
2021-07-23 20:59:16 +0530

    Update task_solution.py

--- task_solution.py
+++ task_solution.py
@@ -1,15 +1,3 @@
-"""
-# Task
-
-You are given an integer K.
-
-Write code to determine a valid integer having K digits such that 
-the product of digits of the integer is greater than or equal to 
-the sum of digits of the integer and the product of the digits of 
-the integer is minimized. If more than 1 such integer exists, 
-determine the smallest possible integer.
-"""
-
 from functools import  reduce
 
 def main(K):
